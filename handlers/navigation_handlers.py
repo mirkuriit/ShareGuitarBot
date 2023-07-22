7bf309e0a3781cf390fabcc829c0efdb3484a6a1
mirkuriit <tot-kto-idet@yandex.ru>
2023-07-22 22:32:00 +0800

    refaactoring

--- handlers/navigation_handlers.py
+++ handlers/navigation_handlers.py
@@ -7,7 +7,7 @@ from repository.redis_pages_repository import PagesRepository
 from repository.material_repository import Materials
 
 from repository.model_formatting_utils import get_tg_list_from_tuple
-from keyboards.inline.kb_generators import navigate_text_kb
+from keyboards.inline.kb_generators import navigate_text_kb, navigate_theory_kb
 
 from filters.no_moving_filter import NoneMovingFilter
 
@@ -19,19 +19,22 @@ m_rep: Materials() = Materials()
 RECORDS_COUNT = 10
 
 
-def get_actual_page(telegram_id: int) -> int:
-    return int(p_rep.get(telegram_id))
+def get_actual_page(telegram_id: int, type:str) -> int:
+    return int(p_rep.get(key=telegram_id,
+                         type=type))
 
 
-@router.callback_query(Text("move_right_text"))
+@router.callback_query(Text("move_right_theory"))
 async def move_text_right_chosen(callback: CallbackQuery, state:FSMContext):
     type = callback.data.split("_")[-1]
     max_page = m_rep.count_user_materials_pages(telegram_id=callback.from_user.id,
-                                               type=type)
-    page = get_actual_page(callback.from_user.id)
+                                                type=type)
+    page = get_actual_page(callback.from_user.id, type=type)
+    print(type)
     if max_page > page:
-        p_rep.increment(callback.from_user.id)
-        page = get_actual_page(callback.from_user.id)
+        p_rep.increment(key=callback.from_user.id,
+                        type=type)
+        page = get_actual_page(callback.from_user.id, type=type)
         result = m_rep.get_own_materials(r_count=RECORDS_COUNT,
                                          type=type,
                                          telegram_id=callback.from_user.id,
@@ -43,7 +46,7 @@ async def move_text_right_chosen(callback: CallbackQuery, state:FSMContext):
                                         page=page)
 
         await callback.message.edit_text(text=f"{answer}",
-                                         reply_markup=navigate_text_kb(
+                                         reply_markup=navigate_theory_kb(
                                          state=True,
                                          page=page
                                          ))
@@ -52,16 +55,17 @@ async def move_text_right_chosen(callback: CallbackQuery, state:FSMContext):
         await callback.answer("Дальше некуда")
 
 
-@router.callback_query(Text("move_left_text"))
+@router.callback_query(Text("move_left_theory"))
 async def move_text_left_chosen(callback: CallbackQuery, state:FSMContext):
     # todo Вынести получение типа в отдельню функцию
     type = callback.data.split("_")[-1]
-    page = int(p_rep.get(callback.from_user.id))
+    page = get_actual_page(callback.from_user.id, type=type)
     if page == 1:
         await callback.answer("Дальше некуда")
     elif page > 1:
-        p_rep.decr(callback.from_user.id)
-        page = int(p_rep.get(callback.from_user.id))
+        p_rep.decr(key=callback.from_user.id,
+                   type=type)
+        page = get_actual_page(callback.from_user.id, type=type)
         result = m_rep.get_own_materials(r_count=RECORDS_COUNT,
                                          type=type,
                                          telegram_id=callback.from_user.id,
@@ -72,7 +76,7 @@ async def move_text_left_chosen(callback: CallbackQuery, state:FSMContext):
                                         r_count=RECORDS_COUNT,
                                         page=page)
         await callback.message.edit_text(text=f"{answer}",
-                                         reply_markup=navigate_text_kb(
+                                         reply_markup=navigate_theory_kb(
                                              state=True,
                                              page=page
                                         ))
